69717e17095a16b8c5f98194a852da7ad098ea7f
David Recheni <davidrecheni@gmail.com>
2023-03-09 12:31:05 +0100

    query shortened

--- src/colony/events.ts
+++ src/colony/events.ts
@@ -4,6 +4,9 @@ import { LogDescription } from "ethers/utils";
 import { Log } from "ethers/providers";
 import { toBigNumber } from "../utils/toBigNumber";
 
+// Shorten the block number to speed up the query
+const fromBlock = 9839527;
+
 const getLogTime = async (blockHash: string | undefined) => {
   if (!blockHash) return -1;
   return await getBlockTime(provider, blockHash);
@@ -30,7 +33,7 @@ export const getEvents = async () => {
   ];
 
   const eventLogs = await Promise.all(
-    filterArray.map((filter) => getLogs(client, filter, {}))
+    filterArray.map((filter) => getLogs(client, filter, { fromBlock }))
   );
 
   const logParser = async (log: Log): Promise<ColonyEvent> => {
